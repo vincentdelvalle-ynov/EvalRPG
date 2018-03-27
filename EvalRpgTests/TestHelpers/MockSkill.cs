6df02518908cb335564f390151f89f67263c90bb
Bybeez <j.foucaud@outlook.fr>
2018-03-27 15:09:48 +0200

    All the work again

---
+++ EvalRpgTests/TestHelpers/MockSkill.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EvalRpgLib.Beings;
+
+namespace EvalRpgTests.TestHelpers
+{
+    public class MockSkill : Skill
+    {
+        public MockSkill(Unit caster)
+            : base(caster)
+        {
+            Name = "Mock Skill";
+            Description = "Dummy description";
+            Cost = 10;
+            BasePower = 1;
+            StatisticReference = StatisticsEnum.Mana;
+            AttributeReference = AttributeEnum.Intelligence;
+
+            Ability = Effect;
+        }
+
+        private bool Effect(Unit dummy)
+        {
+            dummy.StatManager.CurrentStatistics[StatisticsEnum.Health] += 10;
+            return true;
+        }
+
+
+    }
+}
---
+++ EvalRpgTests/skillTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EvalRpgLib.Beings;
+using EvalRpgTests.TestHelpers;
+
+namespace EvalRpgTests
+{
+    [TestClass]
+    public class skillTest
+    {
+        [TestMethod]
+        public void TestCastOK()
+        {
+            Unit A = new Unit("A");
+            A.UpdateStats();
+            int health = A.GetCurrentStat(StatisticsEnum.Health);
+            int mana = A.GetCurrentStat(StatisticsEnum.Mana);
+            Skill skill = new MockSkill(A);
+            skill.Cast(A);
+            Assert.AreEqual(health + 10, A.GetCurrentStat(StatisticsEnum.Health));
+            Assert.AreEqual(mana - 10, A.GetCurrentStat(StatisticsEnum.Mana));
+        }
+
+        [TestMethod]
+        public void TestComputePower()
+        {
+            Unit A = new Unit("A");
+            A.UpdateStats();
+            Skill skill = new MockSkill(A);
+            int result = skill.ComputePower();
+            Assert.AreEqual(11, result);
+        }
+    }
+}
